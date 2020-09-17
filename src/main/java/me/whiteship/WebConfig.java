70e0f018fb687a7b028710fd4c85a4cf725a5367
kyyeto9984 <kyyeto9984@naver.com>
2020-09-17 13:25:45 +0900

    스프링 부트 없이 스프링 WebMvc구현

--- src/main/java/me/whiteship/WebConfig.java
+++ src/main/java/me/whiteship/WebConfig.java
@@ -2,8 +2,22 @@ package me.whiteship;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@ComponentScan()
-public class WebConfig {
+@ComponentScan
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * 여기까지가 스프링 부트 없이 스프링으로 WevMVC를 구현하는 방법
+     * @param registry
+     */
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/", ".jsp");
+    }
 }
